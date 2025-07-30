96d95b0e93d54679444cce454d8e447c5c712e75
Fiona Seaton <fseaton@ceh.ac.uk>
2025-07-30 14:44:49 +0100

    stop plotting index by default

--- R/plot.R
+++ R/plot.R
@@ -7,6 +7,7 @@
 #' @param covariate string giving the name of the matrix-column with covariate values in it
 #' @param index string giving the name of the matrix-column with index values in it
 #' @param widths the widths of the different plots
+#' @param plot_index Whether to plot the index plot, by default \code{FALSE}
 #' @param ... Other arguments passed to [patchwork::wrap_plots()]
 #'
 #'
@@ -29,9 +30,11 @@ lg_plot.sf <- function(data, ...){
 lg_plot.data.frame <- function(data, response = "response",
                                covariate = "covariate",
                                index = "index",
-                               widths = c(0.1, 0.5, 0.5), ...){
+                               widths = c(0.1, 0.5, 0.5),
+                               plot_index = FALSE, ...){
   lg_plot.list(data = data, response = response,
-               covariate = covariate, index = index, widths = widths, ...)
+               covariate = covariate, index = index, widths = widths,
+               plot_index = plot_index, ...)
   # re-arrange the data to be ggplot2-compatible
   # covm <- as.data.frame(data[[covariate]])
   # indm <- as.data.frame(data[[index]])
@@ -64,46 +67,56 @@ lg_plot.data.frame <- function(data, response = "response",
 lg_plot.list <- function(data, response = "response",
                          covariate = "covariate",
                          index = "index",
-                         widths = c(0.1, 0.5, 0.5), ...){
-  if(any(!c(response, covariate, index) %in% names(data))){
-    fl <- !c(response, covariate, index) %in% names(data)
-    stop(paste(paste(c(response, covariate, index)[fl], collapse = ", "),
+                         widths = c(0.1, 0.5, 0.5),
+                         plot_index = FALSE, ...){
+  if(any(!c(response, covariate) %in% names(data))){
+    fl <- !c(response, covariate) %in% names(data)
+    stop(paste(paste(c(response, covariate)[fl], collapse = ", "),
                "is not a named part of data"))
+  }
+  if(!index %in% names(data) & isTRUE(plot_index)){
+    stop(paste(index, "is not a named part of data"))
   }
 
 
   # re-arrange the data to be ggplot2-compatible
   covdata <- as.data.frame(data[[covariate]])
-  inddata <- as.data.frame(data[[index]])
   respdf <- data.frame(resp = data[[response]])
 
   colnames(covdata) <- paste0(covariate,1:ncol(covdata))
-  colnames(inddata) <- paste0(index,1:ncol(inddata))
 
   covdata$.sample_id <- 1:nrow(respdf)
-  inddata$.sample_id <- 1:nrow(respdf)
   respdf$.sample_id <- 1:nrow(respdf)
 
-  covdata <- tidyr::pivot_longer(covdata, !.data$.sample_id,
+  covdata <- tidyr::pivot_longer(covdata, !".sample_id",
                                  values_to = covariate, names_to = "mo")
   covdata$mo <- as.numeric(sub(covariate, "", covdata$mo))
-  inddata <- tidyr::pivot_longer(inddata, !.data$.sample_id,
-                                 values_to = index, names_to = "mo")
-  inddata$mo <- as.numeric(sub(index, "", inddata$mo))
+
+  if(isTRUE(plot_index)){
+    inddata <- as.data.frame(data[[index]])
+    colnames(inddata) <- paste0(index,1:ncol(inddata))
+    inddata$.sample_id <- 1:nrow(respdf)
+    inddata <- tidyr::pivot_longer(inddata, !".sample_id",
+                                   values_to = index, names_to = "mo")
+    inddata$mo <- as.numeric(sub(index, "", inddata$mo))
+  } else{
+    inddata <- NULL
+  }
 
   respdf$.x <- 1
   .make_plot(respdata = respdf, response = "resp",
              covdata = covdata, covariate = covariate,
              inddata = inddata, index = index,
-             widths = widths, ...)
+             widths = widths, plot_index = plot_index, ...)
 }
 
 .make_plot <- function(respdata, response,
                        covdata, covariate,
-                       inddata, index, widths, ...){
+                       inddata, index, widths, plot_index, ...){
   pl <- list(ggplot2::ggplot(respdata) +
-               ggplot2::geom_tile(ggplot2::aes_string(x=".x", y=".sample_id",
-                                                      fill=response)) +
+               ggplot2::geom_tile(ggplot2::aes(x=.data[[".x"]],
+                                               y=.data[[".sample_id"]],
+                                               fill=.data[[response]])) +
                ggplot2::scale_fill_viridis_c() +
                ggplot2::scale_x_continuous(expand=ggplot2::expansion(0)) +
                ggplot2::scale_y_continuous(expand=ggplot2::expansion(0)) +
@@ -112,18 +125,8 @@ lg_plot.list <- function(data, response = "response",
                ggplot2::theme(legend.position="bottom",
                               axis.text.x=ggplot2::element_blank()),
              ggplot2::ggplot(covdata) +
-               ggplot2::geom_tile(ggplot2::aes_string(x="mo", y=".sample_id",
-                                                      fill=covariate)) +
-               ggplot2::scale_fill_viridis_c() +
-               ggplot2::scale_x_continuous(expand=ggplot2::expansion(0)) +
-               ggplot2::scale_y_continuous(expand=ggplot2::expansion(0)) +
-               ggplot2::labs(x=index, y="") +
-               ggplot2::theme_minimal() +
-               ggplot2::theme(legend.position="bottom",
-                              axis.text.y=ggplot2::element_blank()),
-             ggplot2::ggplot(inddata) +
-               ggplot2::geom_tile(ggplot2::aes_string(x="mo", y=".sample_id",
-                                                      fill=index)) +
+               ggplot2::geom_tile(ggplot2::aes(x=.data[["mo"]], y=.data[[".sample_id"]],
+                                               fill=.data[[covariate]])) +
                ggplot2::scale_fill_viridis_c() +
                ggplot2::scale_x_continuous(expand=ggplot2::expansion(0)) +
                ggplot2::scale_y_continuous(expand=ggplot2::expansion(0)) +
@@ -131,6 +134,18 @@ lg_plot.list <- function(data, response = "response",
                ggplot2::theme_minimal() +
                ggplot2::theme(legend.position="bottom",
                               axis.text.y=ggplot2::element_blank()))
+  if(isTRUE(plot_index)){
+    pl[[3]] <- ggplot2::ggplot(inddata) +
+      ggplot2::geom_tile(ggplot2::aes(x=.data[["mo"]], y=.data[[".sample_id"]],
+                                      fill=.data[[index]])) +
+      ggplot2::scale_fill_viridis_c() +
+      ggplot2::scale_x_continuous(expand=ggplot2::expansion(0)) +
+      ggplot2::scale_y_continuous(expand=ggplot2::expansion(0)) +
+      ggplot2::labs(x=index, y="") +
+      ggplot2::theme_minimal() +
+      ggplot2::theme(legend.position="bottom",
+                     axis.text.y=ggplot2::element_blank())
+  }
 
-  patchwork::wrap_plots(pl, ncol=3, widths=widths, ...)
+  patchwork::wrap_plots(pl, ncol=length(pl), widths=widths, ...)
 }
--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -9,14 +9,15 @@ test_that("lg_plot.list works", {
                      index = test_ind),
                 widths = c(1,1,1))
   expect_s3_class(pl, "patchwork")
-  expect_length(pl, 3)
+  expect_length(pl, 2)
 
   pl2 <- lg_plot(list(resp = test_resp,
                       cov = test_cov,
                       ind = test_ind),
                  response = "resp",
                  covariate = "cov",
-                 index = "ind")
+                 index = "ind",
+                 plot_index = TRUE)
   expect_s3_class(pl2, "patchwork")
   expect_length(pl2, 3)
 
@@ -32,7 +33,8 @@ test_df$covariate <- test_cov
 test_df$index <- test_ind
 test_that("lg_plot.data.frame works", {
   pl <- lg_plot(test_df,
-                widths = c(1,1,1))
+                widths = c(1,1,1),
+                plot_index = TRUE)
   expect_s3_class(pl, "patchwork")
   expect_length(pl, 3)
 
@@ -45,10 +47,17 @@ test_that("lg_plot.data.frame works", {
 
 test_sf <- sf::st_as_sf(dplyr::mutate(test_df, X = runif(10), Y = runif(10)),
                         coords = c("X","Y"))
-test_that("lg_plot.data.frame works", {
+test_that("lg_plot.sf works", {
   pl <- lg_plot(test_sf,
-                widths = c(1,1,1))
+                widths = c(1,1,1),
+                index = "dontfail")
   expect_s3_class(pl, "patchwork")
-  expect_length(pl, 3)
+  expect_length(pl, 2)
+
+
+  expect_error(lg_plot(test_sf,
+                       index = "fail", plot_index = TRUE),
+               "not a named part")
+
 
 })
